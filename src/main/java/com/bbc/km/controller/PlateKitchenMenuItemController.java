94b9c4c244caf60060b19af2b3640c4c56586679
alberto.rizzi <alberto.rizzi@carel.com>
2022-08-08 17:17:52 +0200

    add compound for plate item APIs

--- src/main/java/com/bbc/km/controller/PlateKitchenMenuItemController.java
+++ src/main/java/com/bbc/km/controller/PlateKitchenMenuItemController.java
@@ -1,17 +1,60 @@
 package com.bbc.km.controller;
 
-import com.bbc.km.model.PlateKitchenMenuItem;
-import com.bbc.km.service.PlateKitchenMenuItemService;
+import com.bbc.km.compound.CompoundController;
+import com.bbc.km.compound.PlateKitchenMenuItemCompound;
+import com.bbc.km.dto.PlateKitchenMenuItemDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping(path = {"/plate-item"}, consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
-public class PlateKitchenMenuItemController extends RESTController<String, PlateKitchenMenuItem> {
+public class PlateKitchenMenuItemController implements CompoundController<String, PlateKitchenMenuItemDTO> {
 
-    protected PlateKitchenMenuItemController(PlateKitchenMenuItemService service) {
-        super(service);
+    private PlateKitchenMenuItemCompound compound;
+
+    public PlateKitchenMenuItemController(PlateKitchenMenuItemCompound compound) {
+        this.compound = compound;
+    }
+
+    @GetMapping("/{id}")
+    @Override
+    public ResponseEntity<PlateKitchenMenuItemDTO> getById(@PathVariable String id) {
+        return ResponseEntity.ok(compound.getById(id));
+    }
+
+    @GetMapping
+    @Override
+    public ResponseEntity<List<PlateKitchenMenuItemDTO>> getAll() {
+        return ResponseEntity.ok(compound.getAll());
+    }
+
+    @PostMapping
+    @Override
+    public ResponseEntity<List<PlateKitchenMenuItemDTO>> create(@RequestBody List<PlateKitchenMenuItemDTO> pkmiList) {
+        return ResponseEntity.ok(compound.create(pkmiList));
+    }
+
+    @PutMapping
+    @Override
+    public ResponseEntity<List<PlateKitchenMenuItemDTO>> update(@RequestBody List<PlateKitchenMenuItemDTO> pkmiList) {
+        return ResponseEntity.ok(compound.update(pkmiList));
+    }
+
+    @DeleteMapping
+    @Override
+    public ResponseEntity<Map<String, Boolean>> delete(@RequestBody List<String> ids) {
+        return ResponseEntity.ok(compound.delete(ids));
     }
 }
---
+++ src/main/java/com/bbc/km/dto/PlateKitchenMenuItemDTO.java
@@ -0,0 +1,83 @@
+package com.bbc.km.dto;
+
+import com.bbc.km.model.ItemStatus;
+import com.bbc.km.model.KitchenMenuItem;
+import com.bbc.km.model.Plate;
+
+import java.io.Serializable;
+
+public class PlateKitchenMenuItemDTO implements Serializable {
+
+    private String id;
+    private KitchenMenuItem menuItem;
+    private Plate plate;
+    private ItemStatus status;
+    private Integer orderNumber;
+    private Integer tableNumber;
+    private String clientName;
+    private String notes;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public KitchenMenuItem getMenuItem() {
+        return menuItem;
+    }
+
+    public void setMenuItem(KitchenMenuItem menuItem) {
+        this.menuItem = menuItem;
+    }
+
+    public Plate getPlate() {
+        return plate;
+    }
+
+    public void setPlate(Plate plate) {
+        this.plate = plate;
+    }
+
+    public ItemStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ItemStatus status) {
+        this.status = status;
+    }
+
+    public Integer getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(Integer orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public Integer getTableNumber() {
+        return tableNumber;
+    }
+
+    public void setTableNumber(Integer tableNumber) {
+        this.tableNumber = tableNumber;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+}
