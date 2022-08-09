e0aa73ef0f30884611bc6426cee164be55367da5
bozzelliandrea <bozzelliandrea@yahoo.it>
2022-08-09 12:15:06 +0200

    fix: add allowed request methods

--- src/main/java/com/bbc/km/configuration/CORSConfig.java
+++ src/main/java/com/bbc/km/configuration/CORSConfig.java
@@ -2,6 +2,7 @@ package com.bbc.km.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,7 +14,14 @@ public class CORSConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("*")
+                        .allowedMethods(HttpMethod.GET.name(),
+                                HttpMethod.PUT.name(),
+                                HttpMethod.POST.name(),
+                                HttpMethod.PATCH.name(),
+                                HttpMethod.DELETE.name(),
+                                HttpMethod.OPTIONS.name());
             }
         };
     }
