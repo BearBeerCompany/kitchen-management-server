38f88aeb6afd413a5e5c1428e1ac43a33c274d95
bozzelliandrea <bozzelliandrea@yahoo.it>
2022-08-11 22:39:18 +0200

    feat: implemented mongo aggregate func for detailed dto

--- src/main/java/com/bbc/km/repository/PlateKitchenMenuItemRepository.java
+++ src/main/java/com/bbc/km/repository/PlateKitchenMenuItemRepository.java
@@ -1,9 +1,39 @@
 package com.bbc.km.repository;
 
+import com.bbc.km.dto.PlateKitchenMenuItemDTO;
 import com.bbc.km.model.PlateKitchenMenuItem;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PlateKitchenMenuItemRepository extends MongoRepository<PlateKitchenMenuItem, String> {
+
+    @Aggregation(pipeline = {
+        "{'$match':{" +
+            "'_id': '?0'" +
+            "}}",
+        "{'$lookup':{" +
+            "'from': 'plate'," +
+            "'let': {'searchId': {'$toObjectId': '$plateId'}}," +
+            "'pipeline': [{'$match':{'$expr':{'$eq': ['$_id', '$$searchId']}}}]," +
+            "'as': 'plate'" +
+            "}}",
+        "{'$lookup':{" +
+            "'from': 'kitchen_menu_item'," +
+            "'let': {'searchMenuItemId': {'$toObjectId': '$menuItemId'}}," +
+            "'pipeline': [{'$match':{'$expr':{'$eq': ['$_id', '$$searchMenuItemId']}}}]," +
+            "'as': 'menuItem'" +
+            "}}",
+        "{'$project': " +
+            "{'status': 1," +
+                "'notes': 1," +
+                "'clientName': 1," +
+                "'tableNumber': 1," +
+                "'orderNumber': 1," +
+                "'plate': { $arrayElemAt: ['$plate', 0] }" +
+                "'menuItem': { $arrayElemAt: ['$menuItem', 0] }" +
+            "}}"
+    })
+    public PlateKitchenMenuItemDTO findPlateKitchenMenuItemDtoById(String id);
 }
