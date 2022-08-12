1852dca6d3ed146db42fed1ceae2517ea7af9df5
alberto.rizzi <alberto.rizzi@carel.com>
2022-08-12 14:57:08 +0200

    plate counters management for create and update association

--- src/main/java/com/bbc/km/service/PlateKitchenMenuItemService.java
+++ src/main/java/com/bbc/km/service/PlateKitchenMenuItemService.java
@@ -1,14 +1,138 @@
 package com.bbc.km.service;
 
+import com.bbc.km.model.ItemStatus;
+import com.bbc.km.model.Plate;
 import com.bbc.km.model.PlateKitchenMenuItem;
 import com.bbc.km.repository.PlateKitchenMenuItemRepository;
+import com.bbc.km.repository.PlateRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PlateKitchenMenuItemService extends CRUDService<String, PlateKitchenMenuItem> {
 
-    protected PlateKitchenMenuItemService(PlateKitchenMenuItemRepository repository) {
+    private PlateService plateService;
+
+    protected PlateKitchenMenuItemService(PlateKitchenMenuItemRepository repository, PlateService plateService) {
         super(repository);
+        this.plateService = plateService;
+    }
+
+    @Override
+    public PlateKitchenMenuItem create(PlateKitchenMenuItem plateKitchenMenuItem) {
+        Plate plate = null;
+        int currentItems = 0;
+        if (plateKitchenMenuItem.getPlateId() != null) {
+            plate = plateService.getById(plateKitchenMenuItem.getPlateId());
+            if (plate.getEnabled()) {
+                currentItems = plate.getSlot().get(0);
+                int maxItems = plate.getSlot().get(1);
+
+                if (currentItems < maxItems) {
+                    plateKitchenMenuItem.setStatus(ItemStatus.PROGRESS);
+                    currentItems++;
+                }
+            }
+        }
+
+        PlateKitchenMenuItem result = super.create(plateKitchenMenuItem);
+        if (result.getStatus().equals(ItemStatus.PROGRESS)) {
+            plate.getSlot().set(0, currentItems);
+            plateService.update(plate);
+        }
+        return result;
+    }
+
+    @Override
+    public PlateKitchenMenuItem update(PlateKitchenMenuItem plateKitchenMenuItem) {
+        // previous
+        PlateKitchenMenuItem previousItem = this.getById(plateKitchenMenuItem.getId());
+        ItemStatus previousItemStatus = previousItem.getStatus();
+        String previousItemPlateId = previousItem.getPlateId();
+        Plate previousPlate = null;
+        boolean previousPlateChanged = false;
+
+        // next
+        ItemStatus nextItemStatus = plateKitchenMenuItem.getStatus();
+        String nextItemPlateId = plateKitchenMenuItem.getPlateId();
+        Plate nextPlate = null;
+        boolean nextPlateChanged = false;
+
+        if (plateKitchenMenuItem.getPlateId().equals(previousItemPlateId)) {
+            // same plate
+            if (!nextItemStatus.equals(previousItemStatus)) {
+                // status changed
+                nextPlate = plateService.getById(plateKitchenMenuItem.getPlateId());
+                int currentItems = nextPlate.getSlot().get(0);
+                int maxItems = nextPlate.getSlot().get(1);
+
+                if (nextItemStatus.equals(ItemStatus.PROGRESS)) {
+                    // check if almost one slot is free in the current plate
+                    if (currentItems < maxItems) {
+                        // there's a free slot in the current plate, increment the items counter
+                        plateKitchenMenuItem.setStatus(ItemStatus.PROGRESS);
+                        currentItems++;
+                        nextPlate.getSlot().set(0, currentItems);
+                        nextPlateChanged = true;
+                    } else {
+                        // no slot free in current plate, queue the item
+                        plateKitchenMenuItem.setStatus(ItemStatus.TODO);
+                        // todo: notify user that the plate is full
+                    }
+                } else if (previousItemStatus.equals(ItemStatus.PROGRESS)) {
+                    // decrement items counter
+                    currentItems--;
+                    nextPlate.getSlot().set(0, currentItems);
+                    nextPlateChanged = true;
+                }
+            }
+        } else {
+            // item moved from one to plate to another
+            if (previousItemPlateId != null) {
+                if (previousItemStatus.equals(ItemStatus.PROGRESS)) {
+                    // the item was in progress in the previous plate, so decrement its counter
+                    previousPlate = plateService.getById(previousItemPlateId);
+                    int currentItems = previousPlate.getSlot().get(0);
+                    previousPlate.getSlot().set(0, currentItems);
+                    previousPlateChanged = true;
+                }
+            }
+
+            if (nextItemPlateId != null) {
+                // item moved to an existing plate
+                if (nextItemStatus.equals(ItemStatus.PROGRESS)) {
+                    // check if almost one slot is free in the next plate, otherwise queue the item
+                    nextPlate = plateService.getById(plateKitchenMenuItem.getPlateId());
+                    int currentItems = nextPlate.getSlot().get(0);
+                    int maxItems = nextPlate.getSlot().get(1);
+                    if (currentItems < maxItems) {
+                        // free slot in the next plate
+                        currentItems++;
+                        nextPlate.getSlot().set(0, currentItems);
+                        nextPlateChanged = true;
+                    } else {
+                        // no free slot in current plate, queue the item in the next plate
+                        plateKitchenMenuItem.setStatus(ItemStatus.TODO);
+                        // todo: notify user that the plate is full
+                    }
+                }
+            }
+        }
+
+        PlateKitchenMenuItem result = super.update(plateKitchenMenuItem);
+
+        if (previousPlateChanged) {
+            plateService.update(previousPlate);
+        }
+        if (nextPlateChanged) {
+            plateService.update(nextPlate);
+        }
+
+        return result;
+    }
+
+    @Override
+    public PlateKitchenMenuItem delete(String s) {
+        return super.delete(s);
     }
 
     @Override
