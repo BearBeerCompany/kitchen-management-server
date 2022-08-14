c4bfc986f71a22141e91fe4f323f5577ef575c94
bozzelliandrea <bozzelliandrea@yahoo.it>
2022-08-14 15:33:49 +0200

    feat: add banner

--- src/main/java/com/bbc/km/controller/StatsController.java
+++ src/main/java/com/bbc/km/controller/StatsController.java
@@ -1,5 +1,6 @@
 package com.bbc.km.controller;
 
+import com.bbc.km.model.Stats;
 import com.bbc.km.service.StatsService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.text.ParseException;
-import java.util.Date;
+import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -23,8 +24,8 @@ public class StatsController {
     }
 
     @GetMapping
-    public ResponseEntity<?> getInRange(@RequestParam(name = "from") String  from,
-                                        @RequestParam(name = "to", required = false) String to) throws ParseException {
+    public ResponseEntity<List<Stats>> getInRange(@RequestParam(name = "from") String  from,
+                                                  @RequestParam(name = "to", required = false) String to) throws ParseException {
         return ResponseEntity.ok(statsService.get(from, to));
     }
 }
