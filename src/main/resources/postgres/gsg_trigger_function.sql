ea450ab1f7b55f5b0b1461f52df74ff892fcc8c2
Alberto Rizzi <alberto.rizzi@siav.it>
2024-05-02 22:45:08 +0200

    chore: add joins to retrieve menu items, category info, order takeAway

--- src/main/resources/postgres/gsg_trigger_function.sql
+++ src/main/resources/postgres/gsg_trigger_function.sql
@@ -18,18 +18,26 @@ BEGIN
 
 	-- verifico se articolo inserito appartiene a
 	select count(ra.id) into isValidMenuItem
-	from righe_articoli ra
-	where ra.id = new.id
-	and ra.pos_tipologia in (1,2,3,9,12);
+    from righe_articoli ra
+    inner join tipologie t on ra.desc_tipologia = t.descrizione
+    where ra.id = new.id and t.id in (1,2,3,6,8);
 
 	if isValidMenuItem > 0 then
-		select json_build_object('id', ra.id, 'orderNumber', o.progressivo, 'tableNumber', o."numeroTavolo", 'date', o."data", 'time', o.ora, 'clientName', o.cliente, 'orderNotes', o.note,
-			'quantity', r.quantita, 'menuItemName', r.descrizione, 'menuItemNotes', ra.note, 'menuItemExtIndex', ra.posizione)
-		into json_result
-	  	from righe_articoli ra
-	  	inner join righe r on r.id = ra.id_riga
-	  	inner join ordini o on o.id = r.id_ordine
-	  	where ra.id = new.id;
+		select json_build_object(
+        'id', ra.id,
+        'orderNumber', o.progressivo, 'tableNumber', o."numeroTavolo", 'date', o."data", 'time', o.ora, 'clientName', o.cliente,
+        'takeAway', o.esportazione, 'orderNotes', o.note,
+        'quantity', r.quantita,
+        'menuItemId', a.id, 'menuItemName', a.descrizione, 'menuItemNotes', ra.note,
+        'categoryId', t.id, 'categoryName', t.descrizione
+        )
+        into json_result
+        from righe_articoli ra
+        inner join righe r on r.id = ra.id_riga
+        inner join articoli a on r.descrizionebreve = a.descrizionebreve
+        inner join tipologie t on a.id_tipologia = t.id
+        inner join ordini o on o.id = r.id_ordine
+        where ra.id>> = new.id;
 
 	  	RAISE NOTICE 'Risultato JSON %', json_result;
 
