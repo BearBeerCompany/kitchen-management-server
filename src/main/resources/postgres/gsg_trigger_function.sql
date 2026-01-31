c6af729f1d20015c164b5e7caa5b1f7b0a7f076c
Andrea Bozzelli <74464364+bozzelliandrea@users.noreply.github.com>
2026-01-31 16:08:22 +0100

    fix: sql linting

--- src/main/resources/postgres/gsg_trigger_function.sql
+++ src/main/resources/postgres/gsg_trigger_function.sql
@@ -126,16 +126,16 @@ CREATE TABLE orders_ack (
 );
 
 CREATE FUNCTION notify_new_order()
-            RETURNS TRIGGER AS $$
-            BEGIN
-                PERFORM pg_notify('new_order', json_build_object(
-                    'operation', TG_OP,
-                    'item', row_to_json(NEW)
-                )::text);
-                RETURN NEW;
-            END;
-            $$ LANGUAGE plpgsql;
+	RETURNS TRIGGER AS $$
+	BEGIN
+		PERFORM pg_notify('new_order', json_build_object(
+			'operation', TG_OP,
+			'item', row_to_json(NEW)
+		)::text);
+		RETURN NEW;
+	END;
+	$$ LANGUAGE plpgsql;
 
-            CREATE TRIGGER ordini_trigger
-            AFTER INSERT ON ordini
-            FOR EACH ROW EXECUTE FUNCTION notify_new_order();
+CREATE TRIGGER ordini_trigger
+AFTER INSERT ON ordini
+FOR EACH ROW EXECUTE FUNCTION notify_new_order();
