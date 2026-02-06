5a8cd899ff7384eca8748a5fd29883132fc46124
Alberto Rizzi <albe.rizzles@gmail.com>
2026-02-06 18:12:43 +0100

    feat: orders auto insert pt1 (#60)
    * feat: orders auto insert pt1
    
    * feat: orders auto insert pt2

--- src/main/java/com/bbc/km/repository/PlateRepository.java
+++ src/main/java/com/bbc/km/repository/PlateRepository.java
@@ -6,4 +6,27 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PlateRepository extends MongoRepository<Plate, String> {
+
+    @Query("""
+    {
+      'enabled': true,
+      'categories': ?0
+    }
+    """)
+    List<Plate> findCandidatePlates(String categoryId, Sort sort);
+
+    @Query("""
+    {
+      'enabled': true,
+      'categories': ?0,
+      $expr: {
+        $lt: [
+          { $arrayElemAt: ['$slot', 0] },
+          { $arrayElemAt: ['$slot', 1] }
+        ]
+      }
+    }
+    """)
+    List<Plate> findFreePlatesByCategory(String categoryId, Sort sort);
+
 }
