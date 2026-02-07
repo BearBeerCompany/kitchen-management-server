21346191424829a33ebe951fcf96278f8481b76d
Roberto Ceccato <ceck90.rc@gmail.com>
2026-02-07 15:32:03 +0100

    fix: correct query syntax in PlateRepository, add missing imports

--- src/main/java/com/bbc/km/repository/PlateRepository.java
+++ src/main/java/com/bbc/km/repository/PlateRepository.java
@@ -1,8 +1,12 @@
 package com.bbc.km.repository;
 
 import com.bbc.km.model.Plate;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PlateRepository extends MongoRepository<Plate, String> {
@@ -18,7 +22,7 @@ public interface PlateRepository extends MongoRepository<Plate, String> {
     @Query(
         "{ " +
         "  'enabled': true, " +
-        "  'categories': ?0 " +
+        "  'categories': ?0, " +
         "  $expr: {" +
         "    $lt: [" +
         "      { $arrayElemAt: ['$slot', 0] }, " +
